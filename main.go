d402b5d846ffd176a45640ec5fc391063512740d
Gaurav Sablok <codeprog@icloud.com>
2024-09-04 15:29:41 +0200

    codecreatde
    all forms in the HTML finished building up, all router functions added. To add the SQL and the javascript for the print and export. Docker support added so that works on any machine.

--- main.go
+++ main.go
@@ -10,16 +10,53 @@
 package main
 
 import (
-	 "net/http"
-	 "fmt"
-	 "html/template"
-	 "os"
-	 "log"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 func main () {
 
-      http.HandleFunc("/", nil)
-			http.HandleFunc("/prepare", nil)
-			http.HandleFunc("/expediction", nil)
-}
+
+
+	 // a struct for storing the information from the seagrass expedition or experiments
+   type prepare struct {
+		 temp int
+		 benthic string
+		 colletor string
+		 depth string
+		 latitutde string
+		 longitude string
+		 Organization string
+		 site string
+		 date string
+		 time time.Time
+	 }
+
+	 // func to store and process all the experimental layounts
+	 prepareexp := func (w http.http.ResponseWriter, r *http.Request) {
+	      message := r.PostForm("prepareexp")
+				templ := template.Must(template.ParseGlob("../index.html"))
+				templ.Execute(w, ,&prepare)
+
+		 }
+
+	// function to store and process the sequencing runs for the genome
+
+	sequencing : = func (w http.ResponseWriter, r *http.Request) {
+      message := r.PostForm("sequencing")
+			templ := template.Must(template.ParseGlob("../sequencing.html"))
+			templ.Execute(w, &sequencing)
+	}
+
+	    http.HandleFunc("/", nil)
+			http.HandleFunc("/preparesheet",prepare)
+			http.HandleFunc("/sequencing", sequencing)
+
+
+
+
+	}
